621ef64418bb4a8687a709374c4c055b9428b802
Diana Vracheva <diana.vracheva@ampeco.global>
2024-05-20 17:03:52 +0300

    Add required GET params to construct url in CreateCardResponse

--- src/Message/CreateCardResponse.php
+++ src/Message/CreateCardResponse.php
@@ -2,9 +2,10 @@
 
 namespace Ampeco\OmnipayKapitalbank\Message;
 
-class CreateCardResponse extends Response
-{
+use Omnipay\Common\Message\RedirectResponseInterface;
 
+class CreateCardResponse extends Response implements RedirectResponseInterface
+{
     public function isSuccessful() : bool
     {
         return parent::isSuccessful() && $this->data['Response']['Status'] == self::ADD_CARD_STATUS_CODE;
@@ -13,5 +14,40 @@ class CreateCardResponse extends Response
     public function getOrder()
     {
         return $this->data['Response']['Order'];
+    }
+
+    public function isRedirect(): bool
+    {
+        return true;
+    }
+
+    public function getRedirectUrl(): string
+    {
+        return sprintf(
+            "%s?ORDERID=%s&SESSIONID=%s",
+            $this->getUrl(),
+            $this->getOrderId(),
+            $this->getSessionId()
+        );
+    }
+
+    public function getRedirectMethod(): string
+    {
+        return 'GET';
+    }
+
+    private function getUrl(): ?string
+    {
+        return $this->getOrder()['URL'] ?? null;
+    }
+
+    private function getOrderId(): ?string
+    {
+        return $this->getOrder()['OrderID'] ?? null;
+    }
+
+    private function getSessionId(): ?string
+    {
+        return $this->getOrder()['SessionID'] ?? null;
     }
 }
